8f99588379e49c4e560caabcd6c8b59edbb47f08
Ali Zahid <ali.zahid@live.com>
2017-01-30 19:21:55 +0400

    Build script added

---
+++ build.sh
@@ -0,0 +1,4 @@
+cordova build --release --buildConfig ~/Dropbox/Data/android/build-config.json
+cp ./platforms/android/build/outputs/apk/android-release.apk ~/Desktop/boar.apk
+
+open ./platforms/ios
