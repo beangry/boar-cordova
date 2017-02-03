394f7e6ff1c10f337142e509c14f3406ee306c0d
Ali Zahid <ali.zahid@live.com>
2017-02-03 11:18:03 +0400

    Export path updated

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 cordova build --release --buildConfig ~/Dropbox/Data/android/build-config.json
-cp ./platforms/android/build/outputs/apk/android-release.apk ~/Desktop/boar.apk
+cp ./platforms/android/build/outputs/apk/android-release.apk ~/Desktop/boar/boar.apk
 
 open ./platforms/ios
