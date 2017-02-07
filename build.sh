ffc82d0f9caf1726bf2e6094e8ca16028e7f18dd
Ali Zahid <ali.zahid@live.com>
2017-02-07 18:13:15 +0400

    Build script updated

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cordova build --release --buildConfig ~/Dropbox/Data/android/build-config.json
+cordova build android --release --buildConfig ~/Dropbox/Data/android/build-config.json
 cp ./platforms/android/build/outputs/apk/android-release.apk ~/Desktop/boar/boar.apk
 
-open ./platforms/ios
+open ./platforms/ios/Boar.xcodeproj
