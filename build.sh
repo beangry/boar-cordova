ab5a133d12822342d0a9f37d561516ceef1fa87a
Ali Zahid <ali.zahid@live.com>
2017-02-25 05:31:20 +0500

    Script updated
    I’m a potato

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 cordova build android --release --buildConfig ~/Dropbox/Data/android/build-config.json
 cp ./platforms/android/build/outputs/apk/android-release.apk ~/Desktop/boar/boar.apk
 
+cordova build ios
 open ./platforms/ios/Boar.xcodeproj
